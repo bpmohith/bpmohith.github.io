68187c1ff6e6db5a8f5a064b193d8b4ada953727
Mohith BP <30407584+bpmohith@users.noreply.github.com>
2019-11-25 14:58:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+let mySrc = myImage.getAttribute('src');
+if (mySrc === 'images/chrysanthemum.jpg') {
+myImage.setAttribute('src', 'images/penguins.jpg');
+myImage.setAttribute('alt', 'Penguins');
+} else {
+myImage.setAttribute('src', 'images/chrysanthemum.jpg');
+myImage.setAttribute('alt', 'Chrysanthemum');
+
+}
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName () {
+let myName = prompt('Please enter your name:');
+if (!myName || myName ===null) {
+setUserName();
+} else {
+
+localStorage.setItem('name', myName);
+myHeading.textContent = 'Mozilla is cool ' + myName;
+} }
+
+if (!localStorage.getItem('name')) {
+setUserName();
+} else {
+let storedName = localStorage.getItem('name');
+myHeading.textContent = 'Mozilla is cool ' + storedName;
+}
+
+myButton.onclick = function () {
+setUserName();
+}
+
+
+//End of File
